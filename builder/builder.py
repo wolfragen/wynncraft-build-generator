804cf6a86770bc65c621419c4356b712c23b2651
Linksyx <csamcraft@gmail.com>
2026-02-28 00:28:40 +0100

    Can generate - but skill points are flawed

--- builder/builder.py
+++ builder/builder.py
@@ -5,10 +5,10 @@ import json
 
 import json
 
-not_import_stats = ["category", "icon", "name", "drop", "classReq", "lore", "category", "tier"]
+not_import_stats = ["icon", "name", "drop", "classReq", "lore", "tier", "dropInfo", "quest", "armourMaterial"]
 maximized_stats = ["lvl", "strReq", "dexReq", "intReq", "defReq", "agiReq"] # stats where max matters
 build_unique_stats = ["averageDps", "atkSpd"] # stats that only appear once
-item_only_stats = ["id", "displayName", "restrict", "allowCraftsman", "category"] # stats like item displayName that are not used in build stat aggregation
+item_only_stats = ["id", "displayName", "restrict", "allowCraftsman", "category", "type"] # stats like item displayName that are not used in build stat aggregation
 #added_stats = ["nDam", "sdPct", "sdRaw", ...] # stats that can be += like spellDmg - TODO do it properly. TEMP : not in the others
 
 def load_game_data(filepath):
@@ -62,6 +62,7 @@ skill_point_types = ["str", "dex", "int", "def", "agi"]
 
 class BuildInfo:
     def __init__(self, weapon_type, items=None, skill_points=None, available_skill_points = None, exclusive_flags=None):
+        self.weapon_type = weapon_type
         # The 9 slots of a build
         self.items = items or {
             weapon_type: None, "Helmet": None, "Chestplate": None, 
@@ -78,11 +79,14 @@ class BuildInfo:
         # TODO Boolean flags for mutually exclusive items (Hive + Ornate Shadow)
         self.exclusive_flags = exclusive_flags or set()
 
+        self.stats = {}
+
     def add_item(self, slot_name, item_info):
         """
         Returns a new BuildInfo with the item equipped.
         This ensures we don't corrupt the state of parent nodes in the DFS tree.
         """
+        
         item, new_attributions, new_available_skill_points = item_info
 
         new_items = self.items.copy()
@@ -93,29 +97,30 @@ class BuildInfo:
         new_flags = self.exclusive_flags.copy()
         
         # Create the new state
-        new_build = BuildInfo(items=new_items, skill_points=new_sp, exclusive_flags=new_flags)
+        new_build = BuildInfo(self.weapon_type, items=new_items, skill_points=new_sp, exclusive_flags=new_flags)
         
         # We know skill points can be attributed at this point since can_equip was passed
         new_build.available_skill_points = new_available_skill_points
-        new_build.skill_points = copy.deepcopy(self.skill_points)
         for sk in new_attributions.keys():
             new_build.skill_points[sk]["attributed"] += new_attributions[sk]
+
+        for sk in skill_point_types:
+            if sk in item:
+                if item["category"] == "weapon": # TODO Or Crafted item
+                    new_build.skill_points[sk]["items_not_counting"] += stat_to_max(item[sk]) # crafted can have variable skill points
+                else:
+                    new_build.skill_points[sk]["items_counting"] += item[sk]
 
         # TODO add new_build.exclusive_flags based on the newly added item (Hive, Ornate Shadow).
         
         return new_build
     
     def calculate_stats(self):
-        self.stats = {}
-        for item in self.items.values:
-            if item != None:
+        self.stats = {sk:self.skill_points[sk]["attributed"]+self.skill_points[sk]["items_counting"]+self.skill_points[sk]["items_not_counting"] for sk in skill_point_types}
+        self.stats["sdRaw"] = 0; self.stats["sdPct"] = 0; 
+        for item in self.items.values():
+            if item is not None:
                 for stat in item:
-                    # skill points
-                    if stat in skill_point_types:
-                        if item["category"] == "weapon": # TODO Or Crafted item
-                            self.skill_points[stat]["items_not_counting"] += stat_to_max(item[stat]) # crafted can have variable skill points
-                        else:
-                            self.skill_points[stat]["items_counting"] += item[stat]
                     # maximize stat case
                     if stat in maximized_stats: # stats like required lvl and skill Reqs for which only max matters
                         if not stat in self.stats:
@@ -134,7 +139,7 @@ class BuildInfo:
 
 # --- THE SEARCH ALGORITHM ---
 
-def generate_builds(query="", imposed_items=None, weapon_type="Bow"):
+def generate_builds(imposed_items=None, weapon_type="Bow"):
     best_build = None
     best_score = -float('inf')
 
@@ -160,12 +165,18 @@ def generate_builds(query="", imposed_items=None, weapon_type="Bow"):
 
         # Base Case: The build is complete
         if not slots_left:
+            current_build.calculate_stats()
             current_score = score_build_spellDmg(current_build)
+            print(f"Completed a build with score {current_score}")
             if current_score > best_score:
                 best_score = current_score
                 best_build = current_build
                 print(f"New best build found! Score: {best_score}")
-                print(f"Items: {(item['displayName'] for item in best_build.items)}")
+                print(f" - ".join(slot + ": " + item['displayName'] for slot, item in best_build.items.items() if item is not None))
+                #print(f"Skill Points: {best_build.skill_points}")
+                print(f"Available SP: {best_build.available_skill_points}")
+                print(f"Stats: {best_build.stats}")
+                print("------")
             return
 
         # Recursive Step: Try picking a slot to fill next (Iterating orders)
@@ -177,9 +188,10 @@ def generate_builds(query="", imposed_items=None, weapon_type="Bow"):
             # Get candidates for this specific slot at this specific point in the tree
             candidates = search_items(item_type, current_build, score_item_function)
             
-            for item in candidates:
+            for item_info in candidates:
+                #print(f"Trying to equip {item_info[0]['displayName']} in slot {slot_name} with new attributions {item_info[1]} (available: {item_info[2]})")
                 # Equip item and create the next branch of the tree
-                next_build = current_build.add_item(slot_name, item)
+                next_build = current_build.add_item(slot_name, item_info)
                 
                 # Remove the slot we just filled from the remaining list
                 next_slots_left = slots_left[:i] + slots_left[i+1:]
@@ -204,13 +216,13 @@ def stat_to_max(stat):
     return stat
 
 
-def score_build_spellDmg(build_info : BuildInfo, query : ...):
+def score_build_spellDmg(build_info : BuildInfo):
     # TODO use proper formulas with elements and skill points, adjust fields names
     spellRaw = build_info.stats["sdRaw"] if "sdRaw" in build_info.stats else 0
     spellPct = build_info.stats["sdPct"] if "sdPct" in build_info.stats else 0
     dps = build_info.stats["averageDps"] if "averageDps" in build_info.stats else 0
     skStr = build_info.skill_points["str"]["attributed"] + build_info.skill_points["str"]["items_counting"] + build_info.skill_points["str"]["items_not_counting"]
-    return (dps+spellRaw)*(1+spellPct/100)*(1+skStr*(0.7/150)) # approx
+    return max(0, dps+spellRaw)*(1+spellPct/100)*(1+skStr*(0.7/150)) # approx
 
 def score_item_spellDmg(item : dict, partial_build_info : BuildInfo):
     # TODO use proper formulas with elements, adjust fields names
@@ -220,7 +232,7 @@ def score_item_spellDmg(item : dict, partial_build_info : BuildInfo):
     spellPctBuild = partial_build_info.stats["sdPct"] if "sdPct" in partial_build_info.stats else 0
     spellPctItem = stat_to_max(item["sdPct"]) if "sdPct" in item else 0
     dps = partial_build_info.stats["averageDps"] if "averageDps" in partial_build_info.stats else 500 # 500 is not perfect but more representative of a weapon than 0
-    return (dps+spellRawBuild+spellRawItem)*(1+(spellPctBuild+spellPctItem)/100)
+    return max(0, dps+spellRawBuild+spellRawItem)*(1+(spellPctBuild+spellPctItem)/100)
 
 def penalize_skill_point_reqs(item : dict, score : float, penalty_strength : float, partial_build_info : BuildInfo):
     # TODO apply a % penalty from skill points beyond current attribution 
@@ -230,42 +242,58 @@ def penalize_skill_point_reqs(item : dict, score : float, penalty_strength : flo
 def can_equip(item : dict, partial_build_info : BuildInfo):
 
     # TODO: First do Mututally exclusive flags (Hive, Ornate Shadow)
+    # TODO make sure negative skill points are correctly handled
 
-    skill_points_available = partial_build_info.available_skill_points
-    skill_points_counting = {sk:partial_build_info["attributed"][sk]+partial_build_info["items_counting"][sk]
-                             for sk in skill_point_types}
-    skill_points_needed = {sk:item[sk+"Req"] for sk in skill_point_types}
+    skill_points_available = partial_build_info.available_skill_points 
+    skill_points_counting = {sk:partial_build_info.skill_points[sk]["attributed"]+
+                             partial_build_info.skill_points[sk]["items_counting"] 
+                             for sk in skill_point_types} 
+    skill_points_needed = {sk : item[sk+"Req"] if sk+"Req" in item else 0 for sk in skill_point_types}
 
-    # negative skill points must be compensated by attributed ones to keep currently equipped build.
-    if not (item["category"] == "weapon"): # TODO Or Crafted item
-        for sk in skill_point_types:
-            if sk in item and item[sk] < 0:
-                skill_points_needed[sk] -= item[sk] # increases needed skill accordingly
+    # increase needed from possible negative skill points on items
+    #print("Before: " + str(skill_points_needed))
+    for sk in skill_point_types:
+        #if sk in item: print(item[sk], end=" | ")
+        if sk in item and stat_to_max(item[sk]) < 0:
+            skill_points_needed[sk] += abs(stat_to_max(item[sk]))
+    #print("\nAfter: " + str(skill_points_needed))
+            
 
     # check if equippable directly
-    valid = all(skill_points_needed[sk] <= skill_points_counting[sk] for sk in skill_point_types)
-    newly_attributed = None
-    # try to attribute manually
-    if not valid:
-        newly_attributed = {sk:max(0, skill_points_needed[sk]-skill_points_counting[sk]) for sk in skill_point_types}
-        if any(partial_build_info["attributed"][sk]+newly_attributed[sk]>100 for sk in skill_point_types): # todo verify if negative sk allow for more than 100 manual
-            return False, {}, 0 # Needs more than 100 manual skill points
-        skill_points_available -= sum(newly_attributed.values)
-        if skill_points_available < 0:
-            return False, {}, 0 # Not enough skill points    
+    if all(skill_points_needed[sk] <= skill_points_counting[sk] for sk in skill_point_types):
+        return True, {}, skill_points_available
 
-    return valid, newly_attributed, skill_points_available
+    # try to attribute manually
+    newly_attributed = {
+        sk: max(0, skill_points_needed[sk] - skill_points_counting[sk])
+        for sk in skill_point_types
+    }
+
+    # check 100 manuel attribution cap
+    if any(
+        partial_build_info.skill_points[sk]["attributed"] + newly_attributed[sk] > 100
+        for sk in skill_point_types
+    ):
+        return False, {}, 0
+
+    skill_points_available -= sum(newly_attributed.values())
+
+    if skill_points_available < 0:
+        return False, {}, 0
+
+    # If we reach here, it IS equippable
+    return True, newly_attributed, skill_points_available
 
 def search_items(item_type : str, partial_build_info : BuildInfo, score_item_function, range_of_penalty : int = 0):
     # Search items that are equippable and maximize score
     # range of penalty is the number of supplementary items with less restrictive skill requirements to try. 0 is no penalty -> only the "best"
     # returns a list of tuple(item, new_skill_point_attributions, new_available_skill_points)
     max_scores = [(None, 0) for _ in range(range_of_penalty+1)] # (best_item, max_score) for normal max and penalty levels
-    for item in items_database[item_type]:
+    for item in items_database[item_type].values():
         equipable, new_attributions, new_available_skill_points = can_equip(item, partial_build_info)
         item_info = item, new_attributions, new_available_skill_points
         if equipable:
-            score = score_item_function(item_info, partial_build_info)
+            score = score_item_function(item, partial_build_info)
             if score > max_scores[0][1]: # no penalty, pure score
                 max_scores[0] = (item_info, score)
             else: # this else + break avoid duplicates
